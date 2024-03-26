a2ca7b14fc96167bc1fac0bf7381f9f9f0fb3ddd
SadafSamad <164152893+SadafSamad@users.noreply.github.com>
2024-03-26 15:30:24 +0500

    Add files via upload

---
+++ Percentage/main.ts
@@ -0,0 +1,74 @@
+let studentName ="okasha"
+let isSlot ="tuesday"
+let isAllowed = studentName ==="okasha"
+let isAconsole.log(isAllowed);
+
+let isAccepted = studentName === "okasha" && isSlot ==="tuesday"
+console.log(isAllowed)
+
+console.log(isAllowed)
+
+let email ='abc@gmail.com'
+let pass = "fb12345"
+let Allowed = email === "abc@gmail.com" && pass ==='fb12345'
+console.log(isAllowed);
+
+let firstValue = true 
+let secondValue =false
+
+let isallowed = firstValue || secondValue
+ isAllowed =     true     &&   false 
+
+console.log(isAllowed);
+ isAllowed = firstValue || secondValue
+                  true     ||   false
+                  console.log(isAllowed);
+
+     if(5<4){
+       console.log("if wala block");
+     }
+     else {
+       console.log("else wala block");
+     }
+
+     let isPassed = true 
+     if(isPassed){
+       console.log("bike mile gy");
+     }
+
+     else{
+       console.log("sadar jane ke kine huoe");
+     }
+
+     let vehicle ="rickshaw"
+     if(vehicle === "car"){
+       console.log("bike ke parking me jao");
+     }
+     else if(vehicle === "bike"){
+       console.log('bike ke parking me jao');
+     }
+
+     else if(vehicle === "truck"){
+       console.log("truck ke parking me jao");
+     }
+     else {
+       console.log("wapis jao");
+     }
+
+     let percentage = "100"
+     if (percentage === '90' ){
+     console.log('A');
+     }
+     else if (percentage > '80'){
+        console.log('B');
+     }
+     else if (percentage > '60'){
+        console.log('C');
+     }
+     else {
+        console.log('D')
+     }
+
+
+
+
